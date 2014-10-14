2df382e20c28e46aaa99b93c7d2d9adcc68a5ecb
agung212 <biemoary@yahoo.com>
2014-10-14 19:38:31 +0700

    'uploadnewsubsidies'

---
+++ new-subsidies/images/record.php
@@ -0,0 +1,14 @@
+ 
+
+
+function t202initB() { 
+
+	var subid ='29759410';
+	createCookie('tracking202subid',subid,0);
+	
+	var pci = '5297594109';
+	createCookie('tracking202pci',pci,0);
+
+}
+
+t202initB(); 
